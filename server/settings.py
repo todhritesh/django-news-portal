a591668bd178a1095f2936ab77792314ede41837
Dhritesh kumar <todhritesh@gmail.com>
2022-12-13 08:49:07 +0530

    first-commite 1

--- server/settings.py
+++ server/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "cms"
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/"templates" , BASE_DIR/"templates/components"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ server/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+
+def home(req):
+    return render(req , "home.html")
